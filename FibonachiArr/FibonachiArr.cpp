819cf6e0bdafffdb113ffc00f327b97579e9a28e
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-11 22:04:55 +0500

    Добавьте файлы проекта.

---
+++ FibonachiArr/FibonachiArr.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int n = 0, j = 0;
+    const int i = 10;
+    int arr[i];
+    while (j < i) {
+        if (j == 0) {
+            arr[j] = 1;
+            cout << arr[j] << " ";
+        }
+        else {
+            if (j == 1) {
+                arr[j] = 1;
+                cout << arr[j] << " ";
+            }
+            else {
+                arr[j] = arr[j - 1] + arr[j - 2];
+                cout << arr[j] << " ";
+            }
+        }
+        j++;
+    }
+    system("pause > nul");
+    return 0;
+}
